03c8ad11de00f6d9d97263a7d51a8fb2a803309e
Eric John <ericjohneastwood@gmail.com>
2021-03-31 22:44:11 +0530

    Added EngineData object

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -15,6 +15,7 @@ public class MainMenu {
         entry_details[0] = "eric";
         entry_details[1] = "john";
         System.out.println(GenInstructionDB.insert_instruction("source_data_dump", entry_details));
-        Transform t = new Transform();
+        System.out.println(GenInstructionDB.select_one_row_instruction("source_data_dump", "Date_Time"));
+
     }
 }
